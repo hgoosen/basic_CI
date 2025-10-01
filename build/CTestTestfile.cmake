2d60a3ac54c83ed5bd35ac1ad25bf14d182c6a61
Henri Goosen <hgoosen@purdue.edu>
2025-10-01 23:13:19 +0000

    init commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/DELL/Documents/Coding/STM32/CubeSat/basic_CI
+# Build directory: C:/Users/DELL/Documents/Coding/STM32/CubeSat/basic_CI/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/DELL/Documents/Coding/STM32/CubeSat/basic_CI/build/hello_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
